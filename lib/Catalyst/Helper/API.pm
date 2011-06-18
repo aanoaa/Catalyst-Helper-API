82ed5f4e2bfc8e69e90deca2ed736c7f4217747d
Hyungsuk Hong <aanoaa@gmail.com>
2011-06-18 12:14:54 +0900

    testing

--- lib/Catalyst/Helper/API.pm
+++ lib/Catalyst/Helper/API.pm
@@ -1,42 +1,83 @@
 package Catalyst::Helper::API;
-
-# ABSTRACT: a short description for Catalyst::Helper::API
+# ABSTRACT: Helper to create API.pm and congifuration file
 
 use strict;
+use warnings;
+use File::Spec;
 
-=head1 METHODS
+sub mk_stuff {
+    my($self, $helper, @args) = @_;
 
-=head2 mk_compclass
+    # Trait/WithAPI.pm
+    # Trait/WithDBIC.pm
+    # Trait/Log.pm
+    # API.pm
+    # myapp.conf
 
-Make Component Class.
+    $self->mk_traits($helper, @args);
+    $self->mk_api($helper, @args);
+    $self->mk_conf($helper, @args);
 
-=cut
-
-sub mk_compclass {
-    my ( $self, $helper ) = @_;
-    my $file = $helper->{file};
-    $helper->render_file( 'compclass', $file );
 }
 
-=head2 mk_comptest
+sub mk_traits {
+    my($self, $helper, @args) = @_;
+    for my $trait (qw/trait_withapi trait_withdbic trait_log/) {
+        # do something
+    }
+}
 
-Makes test for Controller.
+sub mk_api {
+    my($self, $helper, @args) = @_;
+    my $base = $helper->{base}; # 현재 디렉토리
+    my $app = $helper->{app};   # ex) Foo::Web
 
-=cut
+    $app =~ s/::www$//i;
+    $app =~ s/::web$//ig;
+    $app =~ s/::/\//g;
 
-sub mk_comptest {
-    my ( $self, $helper ) = @_;
-    my $test = $helper->{'test'};
-    $helper->render_file( 'comptest', $test );
+    my $pm = File::Spec->catfile($base, "lib/$app", "API.pm");
+
+    $app =~ s{/}{::}g;
+
+    my $var = {
+        app         => $helper->{app},
+        apis        => \@args || [],
+        namespace   => $app,
+    };
+
+    $helper->mk_dir(File::Spec->catfile($base, "lib/$app"));
+    $helper->render_file('api', $pm, $var);
+}
+
+sub mk_conf {
+    my($self, $helper, @args) = @_;
+
+    my $conf = Catalyst::Utils::appprefix($helper->{app});
+    $helper->render_file('conf', "$conf.conf", $var);
+
+    # share 만들까? 필요없을 듯
+    my $template_conf = File::Spec->catfile( $dist_dir, 'myapp.conf' );
+    my $conf = Catalyst::Utils::appprefix($helper->{app}) . '.conf.new';
+    my $content = slurp $template_conf;
+    my $vars = {
+        lower_name => lc $helper->{name},
+        name => $helper->{name}
+    };
+    $helper->render_file_contents($content, $conf, $vars);
+    print "** please add below lines to your app conf\n";
+    print "# ==========================================\n";
+    print slurp $conf;
+    print "# ==========================================\n";
 }
 
 =head1 SYNOPSIS
 
-    script/create.pl view NAME Catalyst::Helper::API
+    script/create.pl API
 
 =head1 DESCRIPTION
 
-Helper for Catalyst::Helper::API Views.
+Helper for Catalyst::Helper::API
 
 =head1 SEE ALSO
 
@@ -46,46 +87,173 @@ L<Catalyst::Helper>
 
 1;
 
-# PodWeaver complaint 'literal string' about '__DATA__',but mk_XXX subs should have it.
 __DATA__
 
-__compclass__
-package [% class %];
+__api__
+package [% namespace %]::API;
+# ABSTRACT: Auto require [% app %] APIs
+use Moose;
+use namespace::autoclean;
+use [% namespace %]::Schema;
+with qw/[% namespace %]::Trait::WithAPI [% namespace %]::Trait::WithDBIC [% namespace %]::Trait::Log/;
 
-use strict;
-use warnings;
+sub _build_schema {
+    my $self = shift;
+    return [% namespace %]::Schema->connect( $self->connect_info );
+}
 
-use base 'Catalyst::View::TT';
+sub _build_apis {
+    my $self = shift;
+    my %apis;
 
-__PACKAGE__->config(
-    TEMPLATE_EXTENSION => '.tt',
-    render_die => 1,
-);
+    for my $module (qw/[% apis.join(' ') %]/) {
+        my $class = __PACKAGE__ . "::$module";
+        if (!Class::MOP::is_class_loaded($class)) {
+            Class::MOP::load_class($class);
+        }
+        my $opt = $self->opts->{$module} || {};
+        $apis{$module} = $class->new( schema => $self->schema, %{ $opt } );
+    }
 
-=head1 NAME
-
-[% class %] - Catalyst::Helper::API View for [% app %]
-
-=head1 DESCRIPTION
-
-Catalyst::Helper::API View for [% app %].
-
-=head1 SEE ALSO
-
-L<[% app %]>
-
-=head1 AUTHOR
-
-[% author %]
-
-=head1 LICENSE
-
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
+    return \%apis;
+}
 
 1;
 
-__comptest__
-what the test?
+__trait_log__
+package [% namespace %]::Trait::WithAPI;
+# ABSTRACT: Log Trait for [% namespace %]::API Role
+use Moose::Role;
+use namespace::autoclean;
+
+has apis => (
+    is => 'rw',
+    isa => 'HashRef[Object]',
+    lazy_build => 1,
+);
+
+has opts => (
+    is => 'rw',
+    isa => 'HashRef',
+    default => sub { +{} }
+);
+
+sub find {
+    my ($self, $key) = @_;
+    my $api = $self->apis->{$key};
+    if (!$api) {
+        confess "API by key $key was not found for $self";
+    }
+    $api;
+}
+
+no Moose::Role;
+
+1;
+
+__trait_withdbic__
+package [% namespace %]::Trait::WithDBIC;
+# ABSTRACT: DBIC Trait for [% namespace %]::API Role
+use Moose::Role;
+use namespace::autoclean;
+
+has schema => (
+    is => 'ro',
+    lazy_build => 1,
+    handles => {
+        txn_guard => 'txn_scope_guard',
+    }
+);
+
+has connect_info => (
+    is => 'ro',
+    isa => 'HashRef',
+);
+
+has default_moniker => (
+    is => 'ro',
+    isa => 'Maybe[Str]',
+    lazy_build => 1,
+);
+
+has resultset_constraints => (
+    is => 'ro',
+    isa => 'HashRef',
+    predicate => 'has_resultset_constraints',
+);
+
+sub _build_resultset_constraints { return +{} }
+
+sub resultset {
+    my ($self, $moniker) = @_;
+
+    my $schema = $self->schema();
+    $moniker ||= $self->default_moniker;
+    if (! $moniker) {
+        confess blessed($self) . "->resultset() did not receive a moniker, nor does it have a default moniker";
+    }
+
+    my $rs = $schema->resultset($moniker);
+    if ( $moniker eq $self->default_moniker && $self->has_resultset_constraints ) {
+        return $rs->search( $self->resultset_constraints );
+    } else {
+        return $rs;
+    }
+}
+
+no Moose::Role;
+
+1;
+
+__trait_withapi__
+package [% namespace %]::Trait::WithAPI;
+# ABSTRACT: API Trait for [% namespace %]::API Role
+use Moose::Role;
+use namespace::autoclean;
+
+has apis => (
+    is => 'rw',
+    isa => 'HashRef[Object]',
+    lazy_build => 1,
+);
+
+has opts => (
+    is => 'rw',
+    isa => 'HashRef',
+    default => sub { +{} }
+);
+
+sub find {
+    my ($self, $key) = @_;
+    my $api = $self->apis->{$key};
+    if (!$api) {
+        confess "API by key $key was not found for $self";
+    }
+    $api;
+}
+
+no Moose::Role;
+
+1;
+
+__conf__
+<Model API>
+    class   [% namespace %]::API
+    <args> # ARGS of Class Constructor
+        <connect_info>
+            dsn                 **DSN** # dbi:mysql:t_best
+            user                **USERNAME**
+            password            **PASSWORD**
+            RaiseError          1
+            AutoCommit          1
+            mysql_enable_utf8   1
+            on_connect_do       SET NAMES utf8
+        </connect_info>
+        # <opts>
+        #     <System>        # ARGS of [% namespace %]::API::System
+        #         key         value
+        #         key2        another value
+        #     </System>
+        # </opts>
+    </args>
+</Model>
